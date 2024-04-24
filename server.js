4447875f39d04d2e597eaf4556331a8db0b30aa8
Carlos <carlosj.corprez@gmail.com>
2024-04-23 20:10:35 -0400

    deploying backend to render

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ['http://final-project.onrender.com'],
-  })
-);
+app.use(cors());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
